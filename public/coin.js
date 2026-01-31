e86f3657f9703916a49f4768f3f7f67e10951cc0
alexyadav8084-rgb <alexyadav8084@gmail.com>
2026-01-31 23:50:04 +0530

    style

--- public/coin.js
+++ public/coin.js
@@ -42,7 +42,7 @@ coinsData.forEach(item => {
         <div class="space-y-3 coin">
             <div class="border rounded-xl p-4 flex justify-between items-center bg-white hover:border-green-500 transition cursor-pointer" onclick="openPayment(${item.price}, '${item.coins} + ${item.bonus} Bonus')">
                 <div class="flex items-center gap-3">
-                    <span class="text-2xl">🪙</span>
+                    <span class="text-2xl"><img src="https://cdn4.sharechat.com/33d5318_1c8/tools/e7e57ba_1715942283598_sc.webp" class="spin"></span>
                     <div>
                         <p class="font-bold">${item.coins} <span class="text-green-600">+${item.bonus}</span></p>
                         <p class="text-gray-400 text-sm">₹${item.price}  <span class="line-through text-xs">₹${item.oldPrice}</span></p>
